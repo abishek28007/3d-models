c83f3c67ce1de733df7e659e04f6783326eab4ce
Abishek Kumar <abishekk54@gmail.com>
2019-12-05 16:45:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import carScene from './assets/bmw/scene.gltf';
+import ThreeDCarComponent from './components/ThreeDCarComponent';
+import './App.css';
+
+function App() {
+  return (
+    <ThreeDCarComponent/>
+  );
+}
+
+export default App;
