b4233c5dc1347777933444110df5a9f4de93ea63
Abishek <abishekk54@gmail.com>
2019-12-29 20:16:07 +0530

    Moving model component added TODO - add action on model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import carScene from './assets/bmw/scene.gltf';
 // import ThreeDCarComponent from './components/ThreeDCarComponent';
-import MovingCar from './components/MovingCar';
+// import MovingCar from './components/MovingCar';
+import ModelCarMoving from './components/ModelCarMoving';
 import './App.css';
 
 function App() {
   return (
-    <MovingCar/>
+    <ModelCarMoving/>
+    // <MovingCar/>
   );
 }
 
